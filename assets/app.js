67d64a2b898c1f7bf87260ab520d9fe3d5457cba
mabialaelie4@gmail.com <mabialaelie4@gmail.com>
2025-01-15 13:51:06 +0100

    Add header and footer templates, update base HTML and app.js, include new stylesheets

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,8 @@
 import './bootstrap.js';
 
 import './styles/app.css';
+import './styles/header.css';
+import './styles/footer.css';
+
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
